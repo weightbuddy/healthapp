a8a7f5cbe65da5523424c74e31d21d1000bed77f
weightbuddy <adam@weightbuddy.life>
2021-08-18 13:25:23 +1000

    update config file

--- classes/DbConfig.php
+++ classes/DbConfig.php
@@ -1,10 +1,17 @@
 <?php
 class DbConfig 
 {	
-	private $_host = 'localhost';
-	private $_username = 'root';
-	private $_password = '';
-	private $_database = 'hbuddy';
+	private $_host = '203.170.84.9';
+	private $_username = 'stemedu1_bertram';
+	private $_password = "fh9H^g76{.?+";
+	private $_database = 'stemedu1_weightbuddy';
+
+
+	/*My original db info - Open the database & define tables for this website
+$dbserver = "localhost"; my remote IP: 1.136.109.170
+$dbusername = "stemedu1_bertram";
+$dbpassword = "fh9H^g76{.?+";
+$dbname = "stemedu1_weightbuddy";*/
 	
 	protected $connection;
 	
